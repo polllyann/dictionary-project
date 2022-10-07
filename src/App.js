0be76daf6345f371aba242efb5248251da68caef
Anna <108572062+polllyann@users.noreply.github.com>
2022-10-07 18:50:32 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import books from "./books.png";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <img src={books} alt="books"/>
+    Hello
+    </div>
+  );
+}
+
+export default App;
