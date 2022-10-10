f1ee4f9d8bc5522e91797f6ee20ff97daf475b24
Anna <108572062+polllyann@users.noreply.github.com>
2022-10-10 11:00:05 +0200

    fixed bootstrap, added dictionary component and form

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import books from "./books.png";
+import 'bootstrap/dist/css/bootstrap.min.css';
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
-      <img src={books} alt="books"/>
-    Hello
+      <div className="container">
+      <img src={books} alt="books" width={60}/>
+    <main>
+      <Dictionary />
+    </main>
+    <footer className="app-footer">
+      Coded by Anna P.
+    </footer>
+    </div>
     </div>
   );
 }
